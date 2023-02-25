a75f7fa77b3a4314843e263b47198044d4c242e6
Abhinas Dash <abhinasdash143@gmail.com>
2023-02-25 14:19:31 +0530

    react 1st commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import ExpenceItem from './Components/ExpenceItem';
+
+function App() {
+  return (
+    <div>
+      <h2>lets start</h2>
+      <ExpenceItem></ExpenceItem>
+    </div>
+          
+         
+  );
+}
+
+export default App;
---
+++ src/Components/ExpenceItem.js
@@ -0,0 +1,13 @@
+function ExpenceItem(){
+    return (
+        
+        <div>
+            <h1>Expense Items</h1>
+            <div>Food Rs 10</div>
+            <div>Petrol Rs 100</div>
+            <div>Movies Rs 200</div>
+        </div>
+    );
+}
+
+export default ExpenceItem
