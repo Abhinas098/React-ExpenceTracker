9ffc190afba4352efe84018fc167c1eb90308427
Abhinas Dash <abhinasdash143@gmail.com>
2023-02-26 19:39:26 +0530

    making component smaller

--- src/App.js
+++ src/App.js
@@ -8,28 +8,24 @@ function App() {
       title: "Toilet Paper",
       amount: 94,
       date: new Date(2022, 7, 14),
-      LocationOfExpenditure: "Bathroom",
     },
     {
       id: "e2",
       title: "New TV",
       amount: 7000,
       date: new Date(2023, 2, 12),
-      LocationOfExpenditure: "Tv Showroom",
     },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 500,
       date: new Date(2023, 2, 28),
-      LocationOfExpenditure: "Vechicle",
     },
     {
       id: "e4",
       title: "New Desk ",
       amount: 450,
       date: new Date(2021, 5, 12),
-      LocationOfExpenditure: "Wooden",
     },
   ];
 
@@ -38,10 +34,9 @@ function App() {
       {expense.map((expense) => {
         return (
           <ExpenseItem
-            title={expense.title}
+            Title={expense.title}
             Date={(expense.date)}
-            Amount={expense.amount}
-            locationOfExpenditure={expense.LocationOfExpenditure}></ExpenseItem>
+            Amount={expense.amount}></ExpenseItem>
         );
       })}
     </div>
--- src/Components/ExpenceItem.js
+++ src/Components/ExpenceItem.js
@@ -1,18 +1,12 @@
 import "./ExpenceItem.css";
+import ExpenseDate from './ExpenseDate.js';
+import ExpenseDetails from "./ExpenseDetails";
 
 function ExpenceItem(props) {
-  // const expenseDate = new Date(2023, 2, 25);
-  // const expenseTitle = "Car Insurance";
-  // const expenseAmmount = 294.67;
-  // const LocationOfExpenditure = 'Vehicles'
   return (
     <div className="expense-item">
-      <div>{(props.Date).toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-      </div>
-      <div className='expense-item__description'>{props.locationOfExpenditure}</div>
-      <div className="expense-item__price">Rs/-{props.Amount}</div>
+      <ExpenseDate date={props.Date} />
+      <ExpenseDetails title={props.Title} amount={props.Amount} />
     </div>
   );
 }
